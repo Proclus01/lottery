d7bb38d368364c8bd52495f1dd32fd144f8acde3
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-03 21:19:27 -0700

    add first component prototype

---
+++ src/Ball.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Ball.css';
+
+class Ball extends Component {
+    render() {
+        return (
+            <div className="ball">
+                <p className="number">7</p>
+            </div>
+        );
+    }
+}
+
+export default Ball;
